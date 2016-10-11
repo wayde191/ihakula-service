fc32b9dbe1c93cd6952369620d197b275d40176b
wayde191 <wwsun@thoughtworks.com>
2016-10-11 13:18:13 +0800

    for utf-8; Wayde

--- app/stores/tool_store.rb
+++ app/stores/tool_store.rb
@@ -12,7 +12,7 @@ class ToolStore
   def get_validated_neighbours
     begin
       result = []
-      CSV.foreach('uploads/neighbour.csv') do |user|
+      CSV.foreach('uploads/neighbour.csv', 'r:UTF-8') do |user|
         result.push user
       end
       result
